8135f3786d07b0bdd84b1f6cf97aab6db0965fb5
Paul Fox <paul@homemadedigital.com>
2015-08-17 10:51:36 +0100

    New graph provide test

--- graph_provide_test.go
+++ graph_provide_test.go
@@ -3,7 +3,7 @@ package inj
 import "testing"
 
 // Objects passed to a graph should appear in the graph and be populated
-func Test_ProvideHappyPath(t *testing.T) {
+func Test_ProvideHappyPath1(t *testing.T) {
 
 	g, c := NewGraph(), ConcreteType{}
 
@@ -28,8 +28,38 @@ func Test_ProvideHappyPath(t *testing.T) {
 	assertConcreteValue(c, t)
 }
 
+// Multiple calls to Provide shouldh have the same effect as a single call
+func Test_ProvideHappyPath2(t *testing.T) {
+
+	g, c := NewGraph(), ConcreteType{}
+
+	vs := []interface{}{
+		&c,
+		&helloSayer{},
+		&goodbyeSayer{},
+		funcInstance,
+		ichannel,
+		DEFAULT_STRING,
+	}
+
+	// Register providers individually
+	for _, v := range vs {
+		if err := g.Provide(v); err != nil {
+			t.Fatalf("Graph.Provide: %s", err)
+		}
+	}
+
+	// There should be exactly six nodes in the graph now
+	if g, e := len(g.Nodes), 6; g != e {
+		t.Errorf("Got %d nodes, expected %d", g, e)
+	}
+
+	// Check the whole type
+	assertConcreteValue(c, t)
+}
+
 // New dependency provisions shouldn't overwrite previously set ones
-func Test_ProvideOverride(t *testing.T) {
+func Test_ProvideOverride1(t *testing.T) {
 
 	g, c := NewGraph(), ConcreteType{}
 
